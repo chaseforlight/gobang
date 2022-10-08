6d57c5256f01d9fc2c7d198ccd9376224d0d02a7
chaseforlight <114924957+chaseforlight@users.noreply.github.com>
2022-10-08 15:42:49 +0800

    Create intersection.py

---
+++ intersection.py
@@ -0,0 +1,14 @@
+import pygame
+from game_value import GameValue
+
+class Intersection:
+    def __init__(self,center):
+        self.image = pygame.image.load('gobang/images/black_chesspiece.png')
+        self.rect = self.image.get_rect()
+        self.rect.center = center
+
+def create_intersections():
+    for top in range(30,900,60):
+        for left in range(30,900,60):
+            point = Intersection((left,top))
+            GameValue.intersection_list.append(point)
